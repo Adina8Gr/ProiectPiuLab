7d2a5f3a773be7fab192e72a2c2c586d20f8b4a3
Adina <98980934+Didi-Gi@users.noreply.github.com>
2022-03-16 23:42:53 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPiuLaborator
+{
+     public class Persoana
+    {
+        
+        int Id;
+        string nume, prenume, Sex, data_n;
+       
+        public Persoana()
+      {
+            nume=prenume=Sex= string.Empty;
+            Id = 0;
+        }
+        public string retNume()
+        { return nume; }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPiuLaborator
+{
+    public class Camin
+    {
+        string numeCamin, adresa, Administrator, pretCazare;
+        public Camin()
+        { numeCamin=adresa=Administrator=pretCazare= string.Empty; }
+    }
+    
+     class Program
+    {
+        static void Main()
+        {
+            Camin c1 = new Camin();
+            Console.WriteLine(c1);
+
+            Console.ReadKey();
+        }
+    }
+   
+}
